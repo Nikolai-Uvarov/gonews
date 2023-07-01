6303e1853962c719eba626654f2ee284c342a132
Nikolai_Uvarov <nguvarov@gmail.com>
2023-07-01 17:49:21 +0200

    schema.sql updated + postgres pkg updated

--- schema.sql
+++ schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS posts (
 	author_id BIGINT NOT NULL REFERENCES authors(id),
 	title TEXT NOT NULL,
 	content TEXT NOT NULL,
-	created_at BIGINT NOT NULL DEFAULT 0
+	created_at BIGINT NOT NULL DEFAULT 0,
+	published_at BIGINT NOT NULL DEFAULT 0
 );
 
 -- Очистка всех таблиц перед наполнением тестовыми данными.
@@ -20,9 +21,9 @@ TRUNCATE TABLE authors, posts;
 INSERT INTO authors(name) VALUES
     ('Маск'), ('Цукерберг');
 	
-INSERT INTO posts(author_id,title, content, created_at) VALUES
-    (1,'Особенности сражений в Колизее', 'Главное быть готовым к дерзости противника...', 1687180530),
-	(2,'Моя борьба и победа', 'Все начиналось как шоу...', 1687180550),
-	(1,'Держи карман шире', 'Некоторые самоуверенные личности...', 1687180570);
+INSERT INTO posts(author_id,title, content, created_at, published_at) VALUES
+    (1,'Особенности сражений в Колизее', 'Главное быть готовым к дерзости противника...', 1687180530, 1687180550),
+	(2,'Моя борьба и победа', 'Все начиналось как шоу...', 1687180550, 1687180590),
+	(1,'Держи карман шире', 'Некоторые самоуверенные личности...', 1687180570, 1687180580);
 
 
